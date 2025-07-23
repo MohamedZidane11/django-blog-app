1b1c3afde4d8194d67eb4bef49b0d59cc61b1283
Mohamed Zidane <mohamedzidane11@gmail.com>
2025-07-24 00:18:04 +0300

    make migrations and creating superuser(testuser/testuser)

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
+from .models import Article
 
 
 # Register your models here.
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['title', 'content']
--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Article(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
