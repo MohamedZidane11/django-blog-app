79c6ee4febe97a181e7c8ceb7b4ba0611978abe9
Mohamed Zidane <mohamedzidane11@gmail.com>
2025-07-23 23:21:05 +0300

    adding article model view

--- blog/views.py
+++ blog/views.py
@@ -1,2 +1,12 @@
 from django.shortcuts import render
+from .models import Article
 
+# Create your views here.
+def home(request):
+    articles = Article.objects.all().order_by('-created_at')
+    return render(request, 'blog/home.html', {'articles': articles})
+
+
+def article_detail(request, id):
+    article = Article.objects.get(id=id)
+    return render(request, 'blog/article_detail.html', {'article': article})
