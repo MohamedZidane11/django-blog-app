42d429322dc87cd96b5c0153ea6979a6dc97cd08
Mohamed Zidane <mohamedzidane11@gmail.com>
2025-07-26 11:50:20 +0300

    updating view funcs to add search feature

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Article
 
 # Create your views here.
 def home(request):
-    articles = Article.objects.all().order_by('-created_at')
-    return render(request, 'blog/home.html', {'articles': articles})
+    search_query = request.GET.get('search')
+    if search_query:
+        articles = Article.objects.filter(
+            Q(title__icontains=search_query) |
+            Q(content__icontains=search_query)
+        ).order_by('-created_at')
+    else:
+        articles = Article.objects.all().order_by('-created_at')
+    context = {
+        'articles': articles,
+        'search_query': search_query
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def article_detail(request, id):
